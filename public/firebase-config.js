9596322a4c62239751f5a7c355621d48e903ccbf
Ethan <ethansiew03@gmail.com>
2026-09-25 20:10:19 +0800

    Add Firebase-connected signup/login page with BioAudit-matching schema and styling

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyD62tgVngos55efFgP-GG3C0ID0O8onmaM",
+  authDomain: "bioaudit-38ae4.firebaseapp.com",
+  projectId: "bioaudit-38ae4",
+  storageBucket: "bioaudit-38ae4.firebasestorage.app",
+  messagingSenderId: "391854054876",
+  appId: "1:391854054876:web:6feeae9b0c7e4ebe9dc319",
+  measurementId: "G-5CBL8GKX0B"
+};
